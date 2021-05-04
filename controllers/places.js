0310d75cb9f667ed6ae2dc14be5dc6edabfe1a8d
brandiw <brandimichelle.0911@gmail.com>
2021-05-04 10:49:10 -0700

    Can add a new place programmatically

--- controllers/places.js
+++ controllers/places.js
@@ -7,7 +7,18 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places stub')
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
 
 router.get('/new', (req, res) => {
