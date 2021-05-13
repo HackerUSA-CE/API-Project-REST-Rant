c2fa9c46b7136792987971356706e5fb95a26e64
brandiw <brandimichelle.0911@gmail.com>
2021-05-13 16:56:50 -0700

    nav with style

--- views/default.jsx
+++ views/default.jsx
@@ -9,6 +9,16 @@ function Def (html) {
                 <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
