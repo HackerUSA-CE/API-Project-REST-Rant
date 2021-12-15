beebf234f7defd7ecdd7ee5c0b94471bdeed5dac
Allene Norton <norton.allene@gmail.com>
2021-12-15 14:28:37 -0600

    add title and favicon

--- views/default.jsx
+++ views/default.jsx
@@ -4,9 +4,13 @@ function Def (html) {
     return (
         <html>
             <head>
-                <title>Title</title>
+                <title>RestRant Live Demo</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="/css/style.css"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest"></link>
             </head>
             <body>
                 <nav>
